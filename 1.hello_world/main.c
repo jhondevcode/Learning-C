925684c12c46329115682fcd71facc51062c6b3a
jhondev-code <jhondev.code@gmail.com>
2021-01-19 12:34:26 -0500

    Hola mundo fue agregado

---
+++ 1.hello_world/main.c
@@ -0,0 +1,9 @@
+/* Cabeceras de C (libreria estandar) */
+#include <stdio.h>      // funciones de entrada y salida
+#include <stdlib.h>     // constantes y utilidades
+
+/* funcion principal */
+int main() {
+    printf("Hola mundo\n");     // funcion de la libreria <stdio.h>, imprime texto en la consola
+    return EXIT_SUCCESS;        // al ser una funcion entera devielve un numero EXIT_SUCCESS = 0
+}
