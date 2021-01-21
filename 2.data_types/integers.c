0716e6ac6b039a24290543a6556e0c0807b94962
jhondev-code <jhondev.code@gmail.com>
2021-01-20 19:10:55 -0500

    Tipos de datos

---
+++ 2.data_types/integers.c
@@ -0,0 +1,25 @@
+/* Cabeceras de C (libreria estandar) */
+#include <stdio.h>      // funciones de entrada y salida
+#include <stdlib.h>     // constantes y utilidades
+#include <limits.h>     // operaciones para limites de tipos de datos
+#include <float.h>      // operaciones para decimales
+
+/* funcion principal */
+int main() {
+    printf("CHAR_BIT    :   %d\n", CHAR_BIT);
+    printf("CHAR_MAX    :   %d\n", CHAR_MAX);
+    printf("CHAR_MIN    :   %d\n", CHAR_MIN);
+    printf("INT_MAX     :   %d\n", INT_MAX);
+    printf("INT_MIN     :   %d\n", INT_MIN);
+    printf("LONG_MAX    :   %ld\n", (long) LONG_MAX);
+    printf("LONG_MIN    :   %ld\n", (long) LONG_MIN);
+    printf("SCHAR_MAX   :   %d\n", SCHAR_MAX);
+    printf("SCHAR_MIN   :   %d\n", SCHAR_MIN);
+    printf("SHRT_MAX    :   %d\n", SHRT_MAX);
+    printf("SHRT_MIN    :   %d\n", SHRT_MIN);
+    printf("UCHAR_MAX   :   %d\n", UCHAR_MAX);
+    printf("UINT_MAX    :   %u\n", (unsigned int) UINT_MAX);
+    printf("ULONG_MAX   :   %lu\n", (unsigned long) ULONG_MAX);
+    printf("USHRT_MAX   :   %d\n", (unsigned short) USHRT_MAX);
+    return EXIT_SUCCESS;        // al ser una funcion entera devielve un numero EXIT_SUCCESS = 0
+}
