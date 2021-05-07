0fe733b0dfe73c9eda437d8e4f548e114651493c
jhondev-code <jhondev.code@gmail.com>
2021-05-06 21:35:27 -0500

    Template generator has been updated

--- template.py
+++ template.py
@@ -1,5 +1,6 @@
 import sys
 import os
+from datetime import datetime
 
 def create_workspace(dir):
     try:
@@ -11,14 +12,16 @@ def create_workspace(dir):
         with open(f"{dir}/main.c", "w") as file:
             file.write("#include <stdio.h>\n")
             file.write("#include <stdlib.h>\n\n")
+            now = datetime.now()
+            file.write(f"/*\n * Generated on: {now.hour}:{now.minute}:{now.second}\n */\n")
             file.write("int main(int argc, char const *argv[]) {\n")
             file.write(f"{' ' * 4}printf(\"Hello world\\n\");\n")
             file.write(f"{' ' * 4}return EXIT_SUCCESS;\n")
             file.write("}\n")
+            os.system("clear")
+            print("Operation successful")
     except Exception as e:
         print(str(e))
-    os.system("clear")
-    print("Operation successful")
 
 
 if __name__ == '__main__':
