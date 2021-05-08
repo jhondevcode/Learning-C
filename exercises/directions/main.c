418c00c762e5e05f2e14b6508e88dffe5fe8df09
jhondev-code <jhondev.code@gmail.com>
2021-05-07 19:26:56 -0500

    Description added in array pointer

--- exercises/directions/main.c
+++ exercises/directions/main.c
@@ -4,6 +4,9 @@
 int main(int argc, char const *argv[]) {
     int numbers[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
 
+    printf("Array[0] in: %p\n", &numbers[0]);
+    printf("Array start in: %p\n\n", *numbers);
+
     int *start = &numbers[0];
     for (int i = 0; i < 10; i++) {
         printf("%d in -> %p\n", *start, start);
