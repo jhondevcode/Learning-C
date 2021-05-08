c388af0a6455b5f2cf462265c7d3c5a206f2373b
jhondev-code <jhondev.code@gmail.com>
2021-05-07 23:14:02 -0500

    Matrix of decimals

---
+++ 10.matrix/decimals/main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * matrix of decimal
+ * float, double
+ */
+int main(int argc, char const *argv[]) {
+    // Matrix for float
+    float  matrix01[2][3] = {{65.615F, 82.6225F, 861.651F},
+                             {3.1416F, 2.7183F, 31.165F}};
+    // Matrix for double
+    double matrix02[2][2] = {{61461681.861435131D, 413314354.68413143D},
+                             {163841.3654165341D, 68181534.684354114135D}};
+    return EXIT_SUCCESS;
+}
